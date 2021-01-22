d2ae84f1e939f07269b72239a963d4981b2dafd8
Dmytro <dm.vict.kr@gmail.com>
2021-01-22 16:55:57 +0200

    Fix sample `Sample.s`, use dependency name instead of directory, add output

--- sample/Sample.s
+++ sample/Sample.s
@@ -2,7 +2,7 @@
 if( typeof module !== 'undefined' )
 {
   require( 'wTools' );
-  require( '..' /* 'wtemplatefilewriter' */ );
+  require( 'wtemplatefilewriter' );
 }
 
 let _ = wTools;
@@ -19,3 +19,27 @@ var template =
 var writer = new wTemplateFileWriter({ template : template });
 
 writer.form();
+
+console.log( _.toStr( writer ) );
+
+/* log :
+wTemplateFileWriter::
+{
+  dst : '/mnt/home-hdd/ДокумwTemplateFileWriter',
+  srcTemplatePath : null,
+  name : null,
+  resolving : 1
+}
+
+*/
+
+console.log( writer.template );
+
+/* log :
+{
+  'tmp.tmp': {
+    'test1.txt': 'Test file1 content',
+    'test2.txt': 'Test file2 content'
+  }
+}
+*/
